457dfee94e29769689ff8cbbedf8481dd3db947c
らい <yuanleihachi@gmail.com>
2020-09-20 20:50:51 +0900

    First commit to create a SNS App

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,75 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePost = /* GraphQL */ `
+  subscription OnCreatePost {
+    onCreatePost {
+      type
+      id
+      content
+      owner
+      timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePost = /* GraphQL */ `
+  subscription OnUpdatePost {
+    onUpdatePost {
+      type
+      id
+      content
+      owner
+      timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePost = /* GraphQL */ `
+  subscription OnDeletePost {
+    onDeletePost {
+      type
+      id
+      content
+      owner
+      timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateFollowRelationship = /* GraphQL */ `
+  subscription OnCreateFollowRelationship {
+    onCreateFollowRelationship {
+      followeeId
+      followerId
+      timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateFollowRelationship = /* GraphQL */ `
+  subscription OnUpdateFollowRelationship {
+    onUpdateFollowRelationship {
+      followeeId
+      followerId
+      timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteFollowRelationship = /* GraphQL */ `
+  subscription OnDeleteFollowRelationship {
+    onDeleteFollowRelationship {
+      followeeId
+      followerId
+      timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
